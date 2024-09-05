a5fe938367e967e6b243574532e43bfb4f8d739d
Amela <aakija2002@gmail.com>
2024-09-05 21:56:50 +0200

    main page buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import LoginPage from './authentication/LoginPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './authentication/MainPage';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<MainPage />} />
 
       </Routes>
      </Router>
--- src/authentication/MainPage.tsx
+++ src/authentication/MainPage.tsx
@@ -1,1 +1,20 @@
-export class MainPage {}
+import React from 'react';
+import logo from '../assets/logo-web.png';
+import './MainPage.css';
+function MainPage(){
+  return (
+    <div>
+      <button className="login-button">
+        <text className="text-button">Sign in</text>
+      </button>
+      <button className="signup-button">
+        <text className="text-button">Sign up</text>
+      </button>
+      <img src={logo} alt="logo" className="logo" />
+
+
+    </div>
+  )
+}
+
+export default MainPage;
