ef30e8a882bfe9708a2ef47cee91873eb7fa02d2
Amela <aakija2002@gmail.com>
2024-09-23 16:55:13 +0200

    save language

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,11 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-
+const savedLanguage = localStorage.getItem('language') || 'en';
 i18n
   .use(initReactI18next)
   .init({
 
-    lng: "en",
+    lng: savedLanguage,
     fallbackLng: "en",
 
     debug: true,
