00df8cf23744de69d62fc9db9a99f6999d5bafd8
Amela <aakija2002@gmail.com>
2024-11-27 11:26:39 +0100

    test list fixed

--- src/ui-components/test/TestListComponent.tsx
+++ src/ui-components/test/TestListComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TestComponent from './TestComponent';
+import './TestComponent.css';
 
 interface Test {
   testId: string;
@@ -13,7 +14,7 @@ interface TestsListProps {
 
 const TestsList: React.FC<TestsListProps> = ({ tests }) => {
   return (
-    <div>
+    <div className="test-list">
       {tests.map((test) => (
         <TestComponent key={test.testId} test={test} tests={tests} />
       ))}
