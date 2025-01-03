44d2f256bfaa2394477a42e65d6e7efa730e54c2
Amela <aakija2002@gmail.com>
2025-01-03 15:35:10 +0100

    deploy test

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,13 +4,14 @@ import { getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxGjb4Opd8sV7L1LdQzu6RSq1avCqJTpA",
-  authDomain: "animattio-mobile-app.firebaseapp.com",
-  projectId: "animattio-mobile-app",
-  storageBucket: "animattio-mobile-app.appspot.com",
-  messagingSenderId: "202556353541",
-  appId: "1:202556353541:web:4c222878917b5bcebcc06c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
